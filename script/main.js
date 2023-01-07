3a4f6eb161158a1029da70d3463f6c5009decc6f
kabby91 <kabby@kakao.com>
2023-01-07 21:05:24 +0900

    main js add

--- script/main.js
+++ script/main.js
@@ -15,29 +15,8 @@ $(document).ready(function () {
         $("#gift-name").text($(this).data("name"));
     })
 
-
-    $("#reserveGiftButton").click(function () {
-        let name = $("#sender-name").val();
-        let message = $("#sender-message").val();
-        $("#reserveGiftButton").text("전송중...");
-        $("#reserveGiftButton").prop("disabled", true);
-
-        emailjs.init("user_yjLL5xG0A3kkOCH5BGIDh");
-        emailjs.send("wedding-mail", "gift_send", {
-            name: name,
-            gift: $("#gift-name").text(),
-            message: message
-        }).then(function (response) {
-            $('#giftMailModal').modal('hide');
-            alert(name + "님의 메시지가 정상적으로 전송되었습니다.");
-
-            $("#reserveGiftButton").text("예약하기!");
-            $("#sender-name").val('');
-            $("#sender-message").val('');
-            $("#reserveGiftButton").prop("disabled", false);
-        }, function (err) {
-            alert("메시지 전송이 실패했습니다. 다시 시도해주세요.");
-        });
+    $(".groom_call").click(function () {
+        document.location.href='tel:010-7353-8700'
     })
 })
 
